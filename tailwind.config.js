a0a45cc2e8d1ef799d1743f428640fcaf17d872f
dhartline1001 <derien.hartline@gmail.com>
2025-12-30 13:12:46 -0500

    setting up tailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [".src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3a3a3bff",
+        secondary: "#ef0808ff",
+      },
+    },
+  },
+  plugins: [],
+}
+
